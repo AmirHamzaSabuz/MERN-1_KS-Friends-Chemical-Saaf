4729b31028ec3bec61c4234c0078437efc1f1b35
unknown <hamza_amir@yahoo.com>
2020-12-17 12:43:03 +0600

    Fetching Products From The Database

--- backend/data/products.js
+++ backend/data/products.js
@@ -7,7 +7,7 @@ const products =
     description:
     "Tide: America’s Beloved Brand. The results are in. Word of mouth is important when it comes to laundry reviews, and Tide's numbers don't lie. More than 40 million American households used Tide laundry detergent in 2014-2015, and in terms of sales, Tide is the country's number one detergent. ",
     brand: 'Tide',
-    category: 'Cleaning_Items',
+    category: 'Cleaning Items',
     price: 89.99,
     countInStock: 10,
     rating: 4.5,
@@ -20,7 +20,7 @@ const products =
     description:
     'Presenting Surf Excel matic front load liquid detergent - a revolutionary liquid detergent for front load washing machines. Being liquid, it is best suited for washing machines and ensures a superior laundry experience. Surf Excel matic front load liquid detergent is specially made for your front load washing machine and gives you tough stain removal in machines.',
     brand: 'Surf exel',
-    category: 'Cleaning_Items',
+    category: 'Cleaning Items',
     price: 929.99,
     countInStock: 5,
     rating: 3,
@@ -33,7 +33,7 @@ const products =
     description:
     "Gain's first entry into the \"natural\" laundry detergent category the irresistible scent of Gain Botanicals Orange Blossom Vanilla is made from 65% plant-based ingredients! Gentle on skin.",
     brand: 'Gain',
-    category: 'Cleaning_Items',
+    category: 'Cleaning Items',
     price: 399.99,
     countInStock: 11,
     rating: 5,
@@ -46,7 +46,7 @@ const products =
     description:
     'MILD AND PH-BALANCED: Dove body wash includes Moisture Renew Blend—a combination of skin-natural nourishers and plant-based moisturizers that absorb deeply into the top layers of skin',
     brand: 'Dove',
-    category: 'Cleaning_Items',
+    category: 'Cleaning Items',
     price: 399.99,
     countInStock: 11,
     rating: 5,
@@ -59,7 +59,7 @@ const products =
     description:
     'Alcohol free. Citric Acid is plant derived from citrus fruits acts as a Preservative, chelator, pH adjustor.',
     brand: 'Jet',
-    category: 'Cleaning_Items',
+    category: 'Cleaning Items',
     price: 49.99,
     countInStock: 7,
     rating: 3.5,
@@ -72,7 +72,7 @@ const products =
     description:
     'Vim Dishwash Liquid Gel deep cleans the utensils and does not leave any white residue behind unlike dishwash bars',
     brand: 'Vim',
-    category: 'Cleaning_Items',
+    category: 'Cleaning Items',
     price: 29.99,
     countInStock: 0,
     rating: 4,
@@ -85,7 +85,7 @@ const products =
     description:
     "'India's No.1 Floor Cleaning Brand, Recommended by the Indian Medical Association",
     brand: 'Lizol',
-    category: 'Cleaning_Items',
+    category: 'Cleaning Items',
     price: 29.99,
     countInStock: 0, 
     rating: 4,
@@ -98,7 +98,7 @@ const products =
     description:
     'It is 100% organic and natural made with orange and sweet lime peels.Soap free, alcohol free, detergent and chlorine free.',
     brand: 'Vim',
-    category: 'Cleaning_Items',
+    category: 'Cleaning Items',
     price: 29.99,
     countInStock: 0,
     rating: 4,
@@ -111,7 +111,7 @@ const products =
     description:
     'It is 100% organic and natural made with orange and sweet lime peels.Soap free, alcohol free, detergent and chlorine free.',
     brand: 'Dunlop',
-    category: 'Cleaning_Items',
+    category: 'Construction Items',
     price: 29.99,
     countInStock: 0,
     rating: 4,
@@ -124,7 +124,7 @@ const products =
     description:
     'It is 100% organic and natural made with orange and sweet lime peels.Soap free, alcohol free, detergent and chlorine free.',
     brand: 'Harpic',
-    category: 'Cleaning_Items',
+    category: 'Cleaning Items',
     price: 29.99,
     countInStock: 0,
     rating: 4,
@@ -137,7 +137,7 @@ const products =
     description:
     'It is 100% organic and natural made with orange and sweet lime peels.Soap free, alcohol free, detergent and chlorine free.',
     brand: 'Harpic',
-    category: 'Cleaning_Items',
+    category: 'Medical Items',
     price: 29.99,
     countInStock: 0,
     rating: 4,
@@ -150,7 +150,7 @@ const products =
     description:
     'Best Liquid Detergent',
     brand: 'Saaf',
-    category: 'Cleaning_Items',
+    category: 'Cleaning Items',
     price: 65.00,
     countInStock: 0,
     rating: 4,
